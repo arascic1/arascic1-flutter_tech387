2c6f79f2fe93cd0d9e328043edc8b74078b1533f
arascic <arascic1@etf.unsa.ba>
2022-12-01 23:20:55 +0100

    napravljene prve backend rute

--- server/index.js
+++ server/index.js
@@ -5,6 +5,15 @@ const PORT = 3000;
 const express = require('express');
 const app = express();
 
+app.get('/hello-world', (req, res) => {
+    // res.send('hello world');
+    res.json({ val: "zdravo" });
+});
+
+app.get('/', (req, res) => {
+    res.json({ AhmedRascic: "Ahmed Raščić" });
+})
+
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`connected to port ${PORT} hello`);
 });
