4b9ab6c3286ddbc0dfa3ddcb9e365af0ac577337
arascic <arascic1@etf.unsa.ba>
2022-12-01 23:35:15 +0100

    prva izdvojena ruta u zaseban js file

--- server/index.js
+++ server/index.js
@@ -3,8 +3,12 @@ console.log('Hello World');
 const PORT = 3000;
 
 const express = require('express');
+const authRouter = require("./routes/auth.js");
 const app = express();
 
+app.use(authRouter);
+
+/*
 app.get('/hello-world', (req, res) => {
     // res.send('hello world');
     res.json({ val: "zdravo" });
@@ -13,6 +17,7 @@ app.get('/hello-world', (req, res) => {
 app.get('/', (req, res) => {
     res.json({ AhmedRascic: "Ahmed Raščić" });
 })
+*/
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`connected to port ${PORT} hello`);
