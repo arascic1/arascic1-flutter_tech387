1338bc1f9c467784b46bedb4c508c4381985aed5
arascic <arascic1@etf.unsa.ba>
2022-12-01 23:08:10 +0100

    nodemon setup i definisanje js skripti

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
 console.log('Hello World');
+
+const PORT = 3000;
+
+const express = require('express');
+const app = express();
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`connected to port ${PORT} hello`);
+});
